3f4e03c5e3f318789da04a800ddfd1da17097a9a
Ngakan Gandhi <ngandhi@pmintl.net>
2025-08-28 16:59:30 +0700

    feat: adjust main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+mod dataframe;
 mod display;
-mod table;
 mod termsize;
 
-use table::Table;
+use dataframe::DataFrame;
 
-const CSV: &str = "../files/annual-enterprise-survey-2024-financial-year-provisional.csv";
+const CSV: &str = "files/annual-enterprise-survey-2024-financial-year-provisional.csv";
 
 fn main() {
     let us = termsize::get_term_size();
-    println!("{:#?}", us);
-    let tab = Table::new(CSV);
-    let _ = display::display(&tab.unwrap());
+    // println!("{:#?}", us);
+    let tab = DataFrame::new(CSV);
+    // println!("{:#?}", tab);
+    let df = tab.unwrap().data;
+    let _ = display::display(&df);
 }
