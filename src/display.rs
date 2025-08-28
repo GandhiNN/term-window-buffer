543f9f44b857e93980d39efdd104e0c560ee1de8
Ngakan Gandhi <ngandhi@pmintl.net>
2025-08-28 16:59:25 +0700

    feat: display to accept generic type

--- src/display.rs
+++ src/display.rs
@@ -1,11 +1,15 @@
 #![allow(unused)]
-use crate::table::Table;
+use crate::dataframe::DataFrame;
 use crate::termsize;
 use std::error::Error;
+use std::fmt::Debug;
 use std::io;
 use std::io::{Read, Write};
-
-pub fn display(table: &Table) -> Result<(), Box<dyn Error>> {
+use tabled::{
+    Table, Tabled,
+    settings::{Height, Settings, Style, Width, peaker::Priority},
+};
+pub fn display<T: Tabled + Debug>(dataframe: &Vec<Vec<T>>) -> Result<(), Box<dyn Error>> {
     let term_size = termsize::get_term_size().unwrap_or_else(|| {
         println!("Error: something went wrong");
         std::process::exit(1)
@@ -21,12 +25,13 @@ pub fn display(table: &Table) -> Result<(), Box<dyn Error>> {
         let max = current_pos + window_rows - 1;
 
         for i in current_pos..max {
-            if i >= table.data.len() {
+            if i >= dataframe.len() {
                 done = true;
                 break;
             }
-            let line = &table.data[i];
-            println!("{:#?}", line);
+            let line = &dataframe[i];
+
+            println!("{:?}", line);
 
             if i == max - 1 {
                 current_pos = i;
